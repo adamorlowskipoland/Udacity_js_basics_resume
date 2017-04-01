132e919a6c4ac58bcfed14c96d5eeb775f8e2f04
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-01 15:37:07 +0200

    done another small task - find all work locations

--- js/smallTasks.js
+++ js/smallTasks.js
@@ -42,6 +42,16 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
 //var name = "AlbERt EINstEiN";
 //
 //function nameChanger(oldName) {
@@ -64,6 +74,16 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
 //var html = '<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
 //
 //var charEscape = function(_html) {
@@ -82,6 +102,16 @@
 //
 //// Did your code work? The line below will tell you!
 //console.log(charEscape(html));
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -145,6 +175,69 @@
 //// 8property
 //var dotNotation8 = false;
 //var bracketNotation8 = true;
+
+
+
+
+
+
+
+
+//var workOBJ = {
+//  "jobs": [
+//    {
+//      "employer": "Udacity",
+//      "title": "Course Developer",
+//      "location": "Mountain View, CA",
+//      "dates": "Feb 2014 - Current",
+//      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+//    },
+//    {
+//      "employer": "LearnBIG",
+//      "title": "Software Engineer",
+//      "location": "Seattle, WA",
+//      "dates": "May 2013 - Jan 2014",
+//      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+//    },
+//    {
+//      "employer": "LEAD Academy Charter High School",
+//      "title": "Science Teacher",
+//      "location": "Nashville, TN",
+//      "dates": "Jul 2012 - May 2013",
+//      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+//    },
+//    {
+//      "employer": "Stratford High School",
+//      "title": "Science Teacher",
+//      "location": "Nashville, TN",
+//      "dates": "Jun 2009 - Jun 2012",
+//      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+//    }
+//  ]
+//};
+//
+//// Your code goes here! Let me help you get started
+//
+//function locationizer(work_obj) {
+//    const localizations = [];
+//    
+//    for (var localization in work_obj.jobs) {
+//        localizations.push(work_obj.jobs[localization].location);
+//    }
+//    return localizations;
+//}
+//
+//// Did locationizer() work? This line will tell you!
+//console.log(locationizer(workOBJ));
+
+
+
+
+
+
+
+
+
 
 
 
