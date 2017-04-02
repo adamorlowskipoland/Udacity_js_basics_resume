71037cafb0eec2421b5fe3e8e74557436fd993d8
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-02 23:03:48 +0200

    add education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,11 +12,35 @@ var bio = {
     "contacts" : {
         "mobile" : "535580335",
         "email" : "adamorlowskipoland@icloud.com",
-        "github" : "adamorlowskipoland"
+        "github" : "adamorlowskipoland",
+        "location" : "Warsaw"
         },
-    "welcomeMessage" : "Hi, how are you?",
+    "welcomeMessage" : "Hi, I'm looking for my first opportunity as a Developer.",
     "skills" : ["HTML5", "CSS3", "Bootstrap", "JS", "jQuery"],
-    "bioPic" : "../images/fry.jpg"
+    "bioPic" : "../images/fry.jpg",
+    "display" : function() {
+        if (bio.skills !== "") {
+            $('#header').append(HTMLskillsStart);
+
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+            $('#skills').append(formattedSkill);
+
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+            $('#skills').append(formattedSkill);
+
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+            $('#skills').append(formattedSkill);
+
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+            $('#skills').append(formattedSkill);
+
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+            $('#skills').append(formattedSkill);
+
+        } else {
+            console.log("There is no skills");
+        }
+    }
 }
 
 
@@ -24,33 +48,67 @@ var education = {
     "schools" : [
         {
             "name" : "WSB in Torun",
-            "city" : "Torun",
-            "major" : ["Management", "Psychology"]
+            "location" : "Torun",
+            "major" : ["Management", " Psychology"],
+            "dates" : 2011.06
         },
         {
             "name" : "III LO",
-            "city" : "Torun",
-            "major" : ["English", "Maths"]
+            "location" : "Torun",
+            "major" : ["English", " Maths"],
+            "dates" : 2006.06
         }
     ],
     "onlineCourses" : [
         {
             "name" : "JS Design Patterns",
-            "city" : "online",
-            "major" : ["Object Orientet Programming"]
+            "location" : "online",
+            "major" : ["Object Orientet Programming"],
+            "dates" : 2017.02
         },
         {
             "name" : "JS Basics",
-            "city" : "online",
-            "major" : ["JS syntax"]
+            "location" : "online",
+            "major" : ["JS syntax"],
+            "dates" : 2017.01
         },
         {
             "name" : "Frontend Dev Bootcamp",
-            "city" : "Warsaw",
-            "major" : ["HTML5", "CSS3", "Bootstrap", "JS", "jQuery"]
+            "location" : "Warsaw",
+            "major" : ["HTML5", "CSS3", "Bootstrap", "JS", "jQuery"],
+            "dates" : 2016.05
         }
-    ]
+    ],
+    "display" : function () {
+        for (var school in education.schools) {
+            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+
+            $('#education').append(HTMLschoolStart);
+            $('.education-entry:last').append(formattedSchoolName);
+            $('.education-entry:last').append(formattedSchoolDates);
+            $('.education-entry:last').append(formattedSchoolLocation);
+            $('.education-entry:last').append(formattedSchoolMajor);
+        }
+        for (var course in education.onlineCourses) {
+            var formattedCourseName = HTMLschoolName.replace("%data%", education.onlineCourses[course].name);
+            var formattedCourseDates = HTMLschoolDates.replace("%data%", education.onlineCourses[course].dates);
+            var formattedCourseLocation = HTMLschoolLocation.replace("%data%", education.onlineCourses[course].location);
+            var formattedCourseMajor = HTMLschoolMajor.replace("%data%", education.onlineCourses[course].major);
+
+            $('#education').append(HTMLschoolStart);
+            $('.education-entry:last').append(formattedCourseName);
+            $('.education-entry:last').append(formattedCourseDates);
+            $('.education-entry:last').append(formattedCourseLocation);
+            $('.education-entry:last').append(formattedCourseMajor);
+        }
+    }
 };
+
+
+
 
 var work = {
     "jobs" : [
@@ -59,17 +117,32 @@ var work = {
             "title" : "Project Manager",
             "dates" : "2013-2016",
             "location" : "Poland",
-            "workDescription" : ["Management", "Sales"]
+            "description" : ["Management", "Sales"]
         },
         {
             "employer" : "Trops S.A.",
             "title" : "Sales Representative",
             "dates" : "2010-2013",
             "location" : "Kuyavian-Pomeranian Voivodeship",
-            "workDescription" : "Sales"
+            "description" : "Sales"
         }
-    ]
+    ],
+    "display" : function () {
+        for (job in work.jobs) {
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+            $('#workExperience').append(HTMLworkStart);
+            $('.work-entry:last').append(formattedEmployer + formattedTitle);
+            $('.work-entry:last').append(formattedWorkDates + formattedWorkLocation + formattedWorkDescription);
+        }
+    }
 };
+
+
 
 
 var projects = {
@@ -97,43 +170,9 @@ var projects = {
 //$('.biopic:last').src = bio.bioPic;
 //console.log(document.getElementsByClassName('biopic'));
 
-
-if (bio.skills !== "") {
-    $('#header').append(HTMLskillsStart);
-    
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $('#skills').append(formattedSkill);
-    
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $('#skills').append(formattedSkill);
-    
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $('#skills').append(formattedSkill);
-    
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $('#skills').append(formattedSkill);
-    
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
-    $('#skills').append(formattedSkill);
-    
-} else {
-    console.log("There is no skills");
-}
-
-function displayWork() {
-    for (job in work.jobs) {
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-        var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].workDescription);
-
-        $('#workExperience').append(HTMLworkStart);
-        $('.work-entry:last').append(formattedEmployer + formattedTitle);
-        $('.work-entry:last').append(formattedWorkDates + formattedWorkLocation + formattedWorkDescription);
-    }
-}
-displayWork();
+bio.display();
+work.display();
+education.display();
 
 
 projects.display = function () {
